440ac83a41a57007f5fbc144b8856cc6ec792a1e
mrh.exactas <mrh.exactas@3a904882-299d-768f-3d42-9a4b2a7d1252>
2011-09-05 14:25:36 +0000

    El archivo BD-CrecionTablas.sql crea todas las tablas y agrega los PK y FK.
    El archivo BD-InsercionDatos.sql inserta datos en todas las tablas. Los datos ingresados son coherentes con lo que solicita el enunciado, pero hay que restringir algunas cosas con constraints.
    
    El archivo BD-ScriptsVarios.sql tiene una sola consulta que devuelve la antiguedad y todos los datos de los vehiculos y esta armado un filtro para filtrar por la antiguedad.

--- trunk/tp1/scripts/BD-CreacionTablas.sql
+++ trunk/tp1/scripts/BD-CreacionTablas.sql
@@ -2,42 +2,108 @@ use BDtp1Grupo1
 
 -- Creacion de las tablas de la base de datos
 
-create table licencia (nroLicencia int not null primary key, fechaObtencion datetime not null, fechaVencimiento datetime not null, observaciones char(30)) 
+create table gr1_licencia (nroLicencia int not null , fechaObtencion datetime not null, fechaVencimiento datetime not null, observaciones char(30)) 
 
-create table chofer (nroDocumento int not null primary key , fechaNac datetime not null, nomApe char(30) not null, domicilio char(30) not null, telefono int , nroLicencia int not null foreign key references licencia)
+create table gr1_chofer (nroDocumento int not null , fechaNac datetime not null, nomApe char(30) not null, domicilio char(30) not null, telefono int , nroLicencia int not null )
 
-create table viaje (codViaje int not null primary key, fechaHoraPartida datetime , fechaHoraLlegadaEst datetime , nroPatente int , codRecorrido int , realizado bit)
+create table gr1_viaje (codViaje int not null , fechaHoraPartida datetime , fechaHoraLlegadaEst datetime , nroPatente int , codRecorrido int , realizado bit)
 
-create table conduce (nroDocumento  int not null, codViaje int not null)
+create table gr1_conduce (nroDocumento  int not null, codViaje int not null)
 
-alter table conduce add primary key (nroDocumento,codViaje)
-alter table conduce add foreign key (nroDocumento) references chofer
-alter table conduce add foreign key (codViaje) references viaje
+create table gr1_control (codControl int not null , nroDocumento int not null, codViaje int not null, codTipo int not null, resultadoTest bit not null, fechaControl datetime not null)
 
-create table control (codControl int , nroDocumento int ,codViaje int , codTipo int , resultadoTest bit , fechaControl datetime)
+create table gr1_tipoTest (codTipo int not null, descripcion char(30) not null)
 
-create table tipoTest (codTipo int , descripcion char(30))
+create table gr1_contingencia (codContingencia int not null, codViaje int not null, descripcion char(30) not null)
 
-create table contingencia (codContingencia int , codViaje int , descripcion char(30))
+create table gr1_direccion (calle char(30) not null, altura int not null, codCiudad int not null)
 
-create table direccion (calle char(30) , altura int , codCiudad int)
+create table gr1_ciudad ( codCiudad int not null, nombre char(30) not null)
 
-create table ciudad ( codCiudad int , nombre char(30))
+create table gr1_recorrido (codRecorrido int not null, nombre char(30) not null, calleOrig char(30) not null, alturaOrig int not null, codCiudadOrig int not null, calleDest char(30) not null, alturaDest int not null, codCiudadDest int not null)
 
-create table recorrido (codRecorrido int , nombre char(30) , calleOrig char(30) , alturaOrig int , codCiudadOrig int , calleDest char(30) , alturaDest int , codCiudadDest int)
+create table gr1_ruta (codRuta int not null, cantKm int not null, condicionesCamino char(30) , cantPeajes int , tiempoEstimado int , codRecorrido int not null)
+	-- tomamos como que la ruta se puede dar de alta sin tener la cantidad de kilometros, condiciones, cantidad de peajes y tiempo estimado.
 
-create table ruta (codRuta int, cantKm int , condicionesCamino char(30) , cantPeajes int , tiempoEstimado int , codRecorrido int)
+create table gr1_estado (codEstado int not null, descripcion char(30) not null)
 
-create table estado (codEstado int , descripcion char(30))
+create table gr1_vehiculo (nroPatente int not null, modelo char(30) not null, marca char(30) not null, capacidad int not null, fechaAlta datetime not null, codEstado int not null, enUso bit not null, fechaIngresoReparacion datetime)
 
-create table vehiculo (nroPatente int , modelo char(30) , marca char(30) , capacidad int , fechaAlta datetime , codEstado int , enUso bit , fechaIngresoReparacion datetime)
+create table gr1_viajeRealizado (codViaje int not null, fechaHoraLlegada datetime not null, codRuta int not null, codRutaRecorrido int not null)
 
-create table viajeRealizado (codViaje int , fechaHoraLlegada datetime , codRuta int , codRutaRecorrido int)
+create table gr1_participa (codRecorrido int not null, codRuta int not null, nombreEstacion char(30) not null, codClima int not null)
 
-create table participa (codRecorrido int , codRuta int , nombreEstacion char(30), codClima int)
+create table gr1_estacion (nombreEstacion char(30) not null)
 
-create table estacion (nombreEstacion char(30))
+create table gr1_clima (codClima int not null, descripcion char(30) not null)
 
-create table clima (codClima int , descripcion char(30))
+
+-- Agregado de claves primarias
+
+alter table gr1_licencia add primary key (nroLicencia)
+
+alter table gr1_chofer add primary key (nroDocumento)
+
+alter table gr1_conduce add primary key (nroDocumento , codViaje)
+	
+alter table gr1_control add primary key (codControl)
+
+alter table gr1_tipoTest add primary key (codTipo)
+
+alter table gr1_contingencia add primary key (codContingencia , codViaje)
+
+alter table gr1_direccion add primary key (calle , altura , codCiudad)
+
+alter table gr1_ciudad add primary key (codCiudad)
+
+alter table gr1_recorrido add primary key (codRecorrido)
+
+alter table gr1_ruta add primary key (codRuta , codRecorrido)
+
+alter table gr1_estado add primary key (codEstado)
+
+alter table gr1_vehiculo add primary key (nroPatente)
+
+alter table gr1_viaje add primary key (codViaje)
+
+alter table gr1_viajeRealizado add primary key (codViaje)
+
+alter table gr1_participa add primary key (codRecorrido , codRuta , nombreEstacion)
+
+alter table gr1_estacion add primary key (nombreEstacion)
+
+alter table gr1_clima add primary key (codClima)
+
+-- Agregado de claves foraneas
+alter table gr1_chofer add foreign key (nroLicencia) references gr1_licencia
+
+alter table gr1_conduce add foreign key (nroDocumento) references gr1_chofer
+alter table gr1_conduce add foreign key (codViaje) references gr1_viaje
+
+alter table gr1_control add foreign key (nroDocumento) references gr1_chofer
+alter table gr1_control add foreign key (codViaje) references gr1_viaje
+alter table gr1_control add foreign key (codTipo) references gr1_tipoTest
+
+alter table gr1_contingencia add foreign key (codViaje) references gr1_viaje
+
+alter table gr1_direccion add foreign key (codCiudad) references gr1_Ciudad
+
+alter table gr1_recorrido add foreign key (calleOrig , alturaOrig , codCiudadOrig) references gr1_direccion
+alter table gr1_recorrido add foreign key (calleDest , alturaDest , codCiudadDest) references gr1_direccion
+
+alter table gr1_ruta add foreign key (codRecorrido) references gr1_recorrido
+
+alter table gr1_vehiculo add foreign key (codEstado) references gr1_estado
+
+alter table gr1_viaje add foreign key (nroPatente) references gr1_vehiculo
+alter table gr1_viaje add foreign key (codRecorrido) references gr1_recorrido
+
+alter table gr1_viajeRealizado add foreign key (codViaje) references gr1_viaje
+alter table gr1_viajeRealizado add foreign key (codRuta , codRutaRecorrido) references gr1_ruta
+
+alter table gr1_participa add foreign key (codRuta , codRecorrido) references gr1_ruta
+alter table gr1_participa add foreign key (codClima) references gr1_clima
+alter table gr1_participa add foreign key (nombreEstacion) references gr1_estacion
+
 
 
---
+++ trunk/tp1/scripts/BD-ScriptsVarios.sql
@@ -0,0 +1,6 @@
+use BDtp1Grupo1
+--Obtento la antiguedad y todos los datos del vehiculo con antiuedad mayor a XXXX
+declare @filtroAntiguedad int
+-- Setear aca la antiguedad por la cual se quiere filtrar.
+set @filtroAntiguedad = 2
+select(DATEDIFF(yyyy, fechaAlta , GETDATE())) as Antigueadad, * from gr1_vehiculo  where    (DATEDIFF(yyyy, fechaAlta , GETDATE()))>=@filtroAntiguedad 
