bf3f7cdd22f76156d39eab0e7cb39eb2fe942d0a
mrh.exactas <mrh.exactas@3a904882-299d-768f-3d42-9a4b2a7d1252>
2011-09-11 19:31:48 +0000

    Se agrega trigger 'actualizarVehiculo' que impide poner un auto en reparacion si tiene asociado un viaje planificado que todavía no se realizó.
    Agradezco la invaluable ayuda brindaba por el señor Kevin

--- tp1/scripts/BD-AgregadoTriggers.sql
+++ tp1/scripts/BD-AgregadoTriggers.sql
@@ -2,14 +2,13 @@ use BDtp1Grupo1
 -- Triggers
 /*
 	Impide asociar un vehiculo en reparacion a un nuevo viaje planificado.
-
 */
 
 exec ('CREATE TRIGGER nuevoViaje
 ON viajePlanificado
 FOR INSERT
 AS
-/* Chequeo que enUso del vehiculo asociado al viaje sea distinto a 0, para que sea una insercion valida. */
+/* Chequeamos que enUso del vehiculo asociado al viaje sea distinto a 0, para que sea una insercion valida. */
 DECLARE @enUso bit
 SELECT @enUso = v.enUso
 FROM vehiculo v INNER JOIN viajePlanificado vp ON v.nroPatente = vp.nroPatente
@@ -21,14 +20,13 @@ END')
 
 /*
 	Impide modificar un viaje si el mismo tiene asociado un viaje realizado.
-
 */
 
 exec ('CREATE TRIGGER actualizarViaje
 ON viajePlanificado
 FOR update
 AS
-/* Chequeo que enUso del vehiculo asociado al viaje sea distinto a 0, para que sea una insercion valida. */
+/* Chequeamos si existe un viaje realizado para el viaje planificado. Si es así, no permitimos la actualización.*/
 
 if exists (select * from viajeRealizado vr inner join viajePlanificado vp on vr.codViaje = vp.codViaje)
 BEGIN
@@ -36,5 +34,22 @@ BEGIN
    ROLLBACK TRANSACTION
 END')
 
-
-
+/*
+	Impide poner un auto en reparacion si tiene asociado un viaje planificado que todavía no se realizó
+*/
+exec ('CREATE TRIGGER actualizarVehiculo
+ON vehiculo
+FOR update
+AS
+/* Chequeamos si el vehiculo tiene un viaje planificado asociado y no un viaje realizado. Si es así, no permitimos la actualizacion.*/
+if	exists (select * from vehiculo v where enUso=0 and
+		exists(	select * from viajePlanificado vp 
+				where vp.nroPatente = v.nroPatente and 
+				not
+					exists(select * from viajeRealizado vr where vr.codViaje=vp.codViaje)
+				)
+	)
+BEGIN
+   RAISERROR (''No se puede modificar el viaje realizado.'', 16, 1)
+   ROLLBACK TRANSACTION
+END')
