509046bd205990d801dde58de93a27fb8e0a8f13
mrh.exactas <mrh.exactas@3a904882-299d-768f-3d42-9a4b2a7d1252>
2011-09-11 23:04:07 +0000

    Aparentemente funciona "todo".

--- tp1/scripts/BD-CreacionVistas.sql
+++ tp1/scripts/BD-CreacionVistas.sql
@@ -1,59 +1,5 @@
 use BDtp1Grupo1
 
-/* 
-Los recorridos para los cuales se usaron todas las rutas posibles registradas para ese recorrido, 
-para viajes realizados el año pasado y recorridos asociados a mas de una ruta.
-Utiliza las vistas "cantRutasVR" y "cantRutasXRecorrido"
-
-*/
-exec ('create view recorridosConTodasRutasUsadasAnioAnterior as 
-(select  crraa.codRutaRecorrido as codRecorrido , crxr.nombre as recorrido  from cantRutasXRecorrido crxr join cantRutasRecorridasAnioAnterior crraa
-on crxr.recorridoRutas = crraa.codRutaRecorrido where cantRutasRec = rutasRecorridasAnioAnt)')
-
-
-/*
-El promedio de viajes realizados por vehículo por año y el estado en que este se encuentra. Usa la vista "cantViajesXVehiculoXAnio"
-*/
-
-exec ('create view promedioEstadoXVehiculo as (select auxQuery.nroPatente, auxQuery.promedioXAnio, estado.descripcion
-from (
-	-- Este subquery devuelve el promedio de viajes x anio para cada vehiculo
-	select auxView.nroPatente, AVG(auxView.viajesXAnio) as promedioXAnio
-	from cantViajesXVehiculoXAnio as auxView
-	group by auxView.nroPatente
-) as auxQuery
--- Agrego la información del estado actual del vehiculo
-join vehiculo on auxQuery.nroPatente = vehiculo.nroPatente
-join estado on vehiculo.codEstado = estado.codEstado)')
-
-
-/*
-	Los choferes que han utilizado todos los vehículos de menos de dos años de antigüedad, en viajes del último semestre.
-*/
-
-exec ('CREATE view chofer6MVehiculo2A as
-select v.nroDocumento, v.nomApe from viajesChoferesUltimos6Meses v where exists
-(select 1 from antiguedadXVehiculo where antiguedadXVehiculo.antiguedad<2 having count(*)=v.cantVehiculos)')
-
-/*
-	Lista los choferes y cantidad de vehiculos distintos que manejó cada chofer en los ultimos 6 meses.
-*/
-exec ('create view viajesChoferesUltimos6Meses as
-select nroDocumento, nomApe, count(*) as cantVehiculos from
-	(	select distinct c.nroDocumento, c.nomApe, vp.nroPatente
-		from chofer c join conduce co on co.nroDocumento=c.nroDocumento join viajePlanificado vp on vp.codViaje=co.codViaje 
-		where exists (select 1 from viajesUltimos6Meses v6m where v6m.codViaje=vp.codViaje)
-	) as auxQuery
-group by nroDocumento, nomApe')
-
-
-/*
-	Todos los vehiculos usados por cada chofer en los ultimos 6 meses.
-*/
-
-
-exec ('create view viajesUltimos6Meses as
-select * from patenteXviajesRealizados where dateadd(month, 6, fechaHoraLlegada) > getdate() ')
 
 /*
 	Viajes planificados y realizados y patente del vehiculo que realizó el viaje
@@ -62,7 +8,12 @@ select * from patenteXviajesRealizados where dateadd(month, 6, fechaHoraLlegada)
 exec ('create view patenteXviajesRealizados as (select vr.codViaje,vr.fechaHoraLlegada, vp.nroPatente as patente 
 from viajePlanificado vp inner join viajeRealizado vr on vp.codViaje = vr.codviaje)')
 
+/*
+	Todos los vehiculos usados por cada chofer en los ultimos 6 meses.
+*/
 
+exec ('create view viajesUltimos6Meses as
+select * from patenteXviajesRealizados where dateadd(month, 6, fechaHoraLlegada) > getdate() ')
 
 /*
 	Creamos una vista que devuelve el numero de patente y la antiguedad de los vehiculos
@@ -70,7 +21,7 @@ from viajePlanificado vp inner join viajeRealizado vr on vp.codViaje = vr.codvia
 exec ('create view antiguedadXVehiculo as select nroPatente , (DATEDIFF(yy, fechaAlta, GETDATE()) -
 CASE WHEN MONTH(fechaAlta) > MONTH(GETDATE()) OR (MONTH(fechaAlta) =
 MONTH(GETDATE()) AND DAY(fechaAlta) > DAY(GETDATE()))
-THEN 1 ELSE 0 END) as antigueadad from vehiculo')
+THEN 1 ELSE 0 END) as antiguedad from vehiculo')
 
 /*
 Se crea la vista cantRutasVR devuelve el nombre del recorrido, el codigo y la cantidad de rutas asociadas
@@ -104,3 +55,47 @@ CREATE view cantViajesXVehiculoXAnio as
 
 
 
+/* 
+Los recorridos para los cuales se usaron todas las rutas posibles registradas para ese recorrido, 
+para viajes realizados el año pasado y recorridos asociados a mas de una ruta.
+Utiliza las vistas "cantRutasVR" y "cantRutasXRecorrido"
+
+*/
+exec ('create view recorridosConTodasRutasUsadasAnioAnterior as 
+(select  crraa.codRutaRecorrido as codRecorrido , crxr.nombre as recorrido  from cantRutasXRecorrido crxr join cantRutasRecorridasAnioAnterior crraa
+on crxr.recorridoRutas = crraa.codRutaRecorrido where cantRutasRec = rutasRecorridasAnioAnt)')
+
+
+/*
+El promedio de viajes realizados por vehículo por año y el estado en que este se encuentra. Usa la vista "cantViajesXVehiculoXAnio"
+*/
+
+exec ('create view promedioEstadoXVehiculo as (select auxQuery.nroPatente, auxQuery.promedioXAnio, estado.descripcion
+from (
+	-- Este subquery devuelve el promedio de viajes x anio para cada vehiculo
+	select auxView.nroPatente, AVG(auxView.viajesXAnio) as promedioXAnio
+	from cantViajesXVehiculoXAnio as auxView
+	group by auxView.nroPatente
+) as auxQuery
+-- Agrego la información del estado actual del vehiculo
+join vehiculo on auxQuery.nroPatente = vehiculo.nroPatente
+join estado on vehiculo.codEstado = estado.codEstado)')
+
+/*
+	Lista los choferes y cantidad de vehiculos distintos que manejó cada chofer en los ultimos 6 meses.
+*/
+exec ('create view viajesChoferesUltimos6Meses as
+select nroDocumento, nomApe, count(*) as cantVehiculos from
+	(	select distinct c.nroDocumento, c.nomApe, vp.nroPatente
+		from chofer c join conduce co on co.nroDocumento=c.nroDocumento join viajePlanificado vp on vp.codViaje=co.codViaje 
+		where exists (select 1 from viajesUltimos6Meses v6m where v6m.codViaje=vp.codViaje)
+	) as auxQuery
+group by nroDocumento, nomApe')
+
+/*
+	Los choferes que han utilizado todos los vehículos de menos de dos años de antigüedad, en viajes del último semestre.
+*/
+
+exec ('CREATE view chofer6MVehiculo2A as
+select v.nroDocumento, v.nomApe from viajesChoferesUltimos6Meses v where exists
+(select 1 from antiguedadXVehiculo where antiguedadXVehiculo.antiguedad<2 having count(*)=v.cantVehiculos)')
