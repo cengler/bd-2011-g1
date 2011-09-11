9d4227a2d13b1259350e9562beb21bf78f22aeea
mrh.exactas <mrh.exactas@3a904882-299d-768f-3d42-9a4b2a7d1252>
2011-09-11 16:21:17 +0000

    se borra lo que se pasó como vista

--- tp1/scripts/BD-ScriptsVarios.sql
+++ tp1/scripts/BD-ScriptsVarios.sql
@@ -5,29 +5,3 @@ declare @filtroAntiguedad int
 -- Setear aca la antiguedad por la cual se quiere filtrar.
 set @filtroAntiguedad = 2
 select(DATEDIFF(yyyy, fechaAlta , GETDATE())) as Antigueadad, * from vehiculo  where    (DATEDIFF(yyyy, fechaAlta , GETDATE()))>=@filtroAntiguedad 
-
-/* 
-Los recorridos para los cuales se usaron todas las rutas posibles registradas para ese recorrido, 
-para viajes realizados el año pasado y recorridos asociados a mas de una ruta.
-Utiliza las vistas "cantRutasVR" y "cantRutasXRecorrido"
-
-*/
-select  crraa.codRutaRecorrido as codRecorrido , crxr.nombre as recorrido  from cantRutasXRecorrido crxr join cantRutasRecorridasAnioAnterior crraa
-on crxr.recorridoRutas = crraa.codRutaRecorrido where cantRutasRec = rutasRecorridasAnioAnt
-
-
-/*
-El promedio de viajes realizados por vehículo por año y el estado en que este se encuentra. Usa la vista "cantViajesXVehiculoXAnio"
-*/
-
-select auxQuery.nroPatente, auxQuery.promedioXAnio, estado.descripcion
-from (
-	-- Este subquery devuelve el promedio de viajes x anio para cada vehiculo
-	select auxView.nroPatente, AVG(auxView.viajesXAnio) as promedioXAnio
-	from cantViajesXVehiculoXAnio as auxView
-	group by auxView.nroPatente
-) as auxQuery
--- Agrego la información del estado actual del vehiculo
-join vehiculo on auxQuery.nroPatente = vehiculo.nroPatente
-join estado on vehiculo.codEstado = estado.codEstado
-
