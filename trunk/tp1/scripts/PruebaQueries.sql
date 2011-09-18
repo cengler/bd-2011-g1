69c0e85e589cee7a0afd9dfea71a516beac45cb1
marcoskevin@gmail.com <marcoskevin@gmail.com@3a904882-299d-768f-3d42-9a4b2a7d1252>
2011-09-18 05:27:19 +0000

    Nuevas modificaciones al query que trae todos los recorridos para los cuales se usaron todas las rutas en viajes realizados el año pasado. Este query no usa subqueries en el from pero si en el where cuando utiliza los "not exists"

--- trunk/tp1/scripts/PruebaQueries.sql
+++ trunk/tp1/scripts/PruebaQueries.sql
@@ -15,13 +15,11 @@ from recorrido
 where not exists
 (	select ruta.nroRuta
 	from ruta
-	where not exists
+	where ruta.codRecorrido=recorrido.codRecorrido
+	and not exists
 	(	select nroRuta,codRecorrido,fechaHoraLlegada
-		from viajeRealizado vR inner join viajePlanificado vP
-		on vR.codViaje=vP.codViaje
-		where vR.nroRuta=ruta.nroRuta and vP.codRecorrido=recorrido.codRecorrido
-		--and (2010 = year(vR.fechaHoraLlegada))
-		--and (year(GETDATE()) - year(fechaHoraLlegada))<=1
-		and (DATEDIFF(yy, fechaHoraLlegada, GETDATE())<=1)
+		from viajeRealizado vR
+		where vR.nroRuta=ruta.nroRuta and vR.codRutaRecorrido=ruta.codRecorrido
+		and (DATEDIFF(yy, fechaHoraLlegada, GETDATE())=1)
 	)
 )
