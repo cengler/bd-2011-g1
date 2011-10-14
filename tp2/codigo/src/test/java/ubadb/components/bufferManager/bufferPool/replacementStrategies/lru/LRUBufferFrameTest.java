c903f741c7724d30131e9d2b8e7bb133baca8eff
caeycae <caeycae@3a904882-299d-768f-3d42-9a4b2a7d1252>
2011-10-14 18:30:06 +0000

    arreglo los test de unidad -- FALTA el tema de reiniciar el ACCESS_COUNT actualizacion del informe (implementacion/correcciones caratula)

--- tp2/codigo/src/test/java/ubadb/components/bufferManager/bufferPool/replacementStrategies/lru/LRUBufferFrameTest.java
+++ tp2/codigo/src/test/java/ubadb/components/bufferManager/bufferPool/replacementStrategies/lru/LRUBufferFrameTest.java
@@ -17,12 +17,14 @@ public class LRUBufferFrameTest
 		LRUBufferFrame bufferFrame1 = new LRUBufferFrame(MockObjectFactory.PAGE);
 		
 		bufferFrame0.pin();
+		bufferFrame0.unpin();
 		bufferFrame1.pin();
+		bufferFrame1.unpin();
 		bufferFrame0.pin();
 		
-		assertTrue(bufferFrame0.getPinCount() == 2);
-		assertTrue(bufferFrame1.getPinCount() == 1);
-		assertTrue(bufferFrame0.getAccessCounter() == 5);
-		assertTrue(bufferFrame1.getAccessCounter() == 4);
+		assertTrue("bufferFrame0.getPinCount()!=2", bufferFrame0.getPinCount() == 1);
+		assertTrue("bufferFrame1.getPinCount()!=1", bufferFrame1.getPinCount() == 0);
+		assertTrue("bufferFrame0.getAccessCounter()!=5", bufferFrame0.getAccessCounter() == 3);
+		assertTrue("bufferFrame1.getAccessCounter()!=4", bufferFrame1.getAccessCounter() == 4);
 	}
 }
